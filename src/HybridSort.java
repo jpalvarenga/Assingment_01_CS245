ef0dbc9831be6367d5277a289a5472914341d0c1
Joao Alvarenga <39594289+jpalvarenga@users.noreply.github.com>
2018-10-23 12:31:06 -0700

    Comments and changes.

--- src/HybridSort.java
+++ src/HybridSort.java
@@ -1,22 +1,30 @@
 import java.util.ArrayList;
 
 public class HybridSort implements SortingAlgorithm{
-	
-	
-	public static ArrayList<int[]> runs = new ArrayList<int[]>();
 
+	/**
+	 * Method calls findRuns which finds all the runs and separate them in different arrays. 
+	 * It calls the method merge which merges all the runs with the merge sort algorithm.
+	 * It copies all the elements in the result array into the original array.
+	 */
 	@Override
 	public void sort(int[] a) {
-		runs(3, a);
+		var runs = findRuns(3, a);
+		var result = merge(runs);
 		
-		var array = runs.get(0);
-		for(int i=0; i< array.length;i++){
-            a[i] = array[i];
-        }
-		
+		for(int i = 0; i < result.length; i++) {
+			a[i] = result[i];
+		}
 	}
-	
-	public void runs(int min, int[] arr) {
+	/**
+	 * This method finds all the runs within an array with a minimum array size of @param min.
+	 * @param min is the minimum array size to look for a sequence of numbers that is bigger than previous number on the list.
+	 * @param arr is the array to find the runs from
+	 * @return returns an ArrayList containing all the runs.
+	 */
+	public ArrayList<int[]> findRuns(int min, int[] arr) {
+		
+		var runs = new ArrayList<int[]>();
 		
 		int first = 0;
 		int previous;
@@ -29,7 +37,7 @@ public class HybridSort implements SortingAlgorithm{
 			if(arr[previous] > arr[current]) {
 				if(longEnough(first, previous, min)) {
 					runs.add(subarray(first, previous, arr));
-					splitArray(pointer, first, min, arr);
+					splitArray(pointer, first, min, arr, runs);
 					pointer = current;
 				}
 				first = current;
@@ -38,26 +46,34 @@ public class HybridSort implements SortingAlgorithm{
 			if(current == arr.length - 1){
 				if(longEnough(first, current, min)) {
 					runs.add(subarray(first, current, arr));
-					splitArray(pointer, first, min, arr);
+					splitArray(pointer, first, min, arr, runs);
 					pointer = current;
 				}else {
-					splitArray(pointer, current + 1, min, arr);
+					splitArray(pointer, current + 1, min, arr, runs);
 				}
 			}
 			
 		}
 		
-		merge(new ArrayList<int[]>());
-		
+		return runs;
 		
 	}
 	
-	public static void merge(ArrayList<int[]> temp) {
+	/**
+	 * Merge merges all the arrays found in @param runs
+	 * 
+	 * @param runs ArrayList of runs
+	 * @return return merged ArrayList.
+	 */
+	public static int[] merge(ArrayList<int[]> runs) {
 		
 		
 		if(runs.size() == 1) {
-			return;
+			return runs.get(0);
 		}
+		
+		ArrayList<int[]> temp = new ArrayList<int[]>();
+		
 		int previous;
 		for(int current = 1; current < runs.size(); current = current + 2) {
 			previous = current - 1;
@@ -68,12 +84,16 @@ public class HybridSort implements SortingAlgorithm{
 			temp.add(runs.get(runs.size() - 1));
 		}
 		
-		runs = temp;
-		
-		merge(new ArrayList<int[]>());
+		return merge(temp);
 		
 	}
-	
+	/**
+	 * This method merges an array to the other with a a MergeSort algorithm.
+	 * 
+	 * @param L the left array
+	 * @param R the right array
+	 * @return returns merged array.
+	 */
 	public static int[] merge(int[] L, int[] R) {
 
 			int n1 = L.length;
@@ -108,16 +128,30 @@ public class HybridSort implements SortingAlgorithm{
 	            j++;
 	            k++;
 	        }
-	        
 	        return arr;
-		
 	}
 	
+	/**
+	 * Checks if the left pointer (@param l) and the right pointer (@param h) are far enough apart.
+	 * @param l left pointer
+	 * @param h right pointer
+	 * @param min how far they should be apart
+	 * @return true if they are far enough.
+	 */
 	public static boolean longEnough(int l, int h, int min) {
 		return (h-l) + 1 >= min;
 	}
 	
-	public static void splitArray(int start , int limit, int size, int[] arr) {
+	/**
+	 * This method finds runs that are not ordered from pointer ( @param start ) to the other pointer ( @param limit )
+	 * from an array ( @param arr ). It adds it to the ArrayList of runs that was found
+	 * @param start starting pointer
+	 * @param limit ending pointer
+	 * @param size the limited size the runs should be
+	 * @param arr the array from which to look for
+	 * @param runs the result of the runs found
+	 */
+	private static void splitArray(int start , int limit, int size, int[] arr, ArrayList<int[]> runs) {
 		int current = start;
 		int length;
 		while(current != limit) {
@@ -130,12 +164,6 @@ public class HybridSort implements SortingAlgorithm{
 		}
 	}
 	
-	
-	
-	
-	
-
-
 	/**
 	 * This method returns a sub array of an array according to the lowest and highest elements
 	 * passed in as a parameter.
@@ -159,9 +187,12 @@ public class HybridSort implements SortingAlgorithm{
 		
 		insertionSort(copy);
 		return copy;
-		
 	}
 	
+	/**
+	 * Insertion Sort algorithm
+	 * @param a is the array from which we should sort.
+	 */
 	public static void insertionSort(int[] a) {
         for(int i = 1; i < a.length; i++) {
             int current = a[i];
@@ -174,6 +205,5 @@ public class HybridSort implements SortingAlgorithm{
             }
         }
     }
-	
 	
 }
